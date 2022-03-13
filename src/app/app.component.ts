c92d5f73fc2286c62d84c30cfec57b1610cf30c7
qiqiqiyaya <757983029@qq.com>
2022-03-13 22:31:35 +0800

    with echart test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import domToImage from 'dom-to-image';
+import { EChartsOption } from 'echarts';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-DOM-to-Image-test';
+  options:EChartsOption = {
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+      },
+    ],
+  };
+
+  @ViewChild('container', { static: true }) container: any;
+
+  ngOnInit() {
+
+  }
+
+  ngAfterViewInit(){
+    setTimeout(() => {
+      debugger;
+      domToImage.toPng(this.container.nativeElement)
+        .then(function (dataUrl: any) {
+          debugger;
+          var img = new Image();
+          img.src = dataUrl;
+          document.body.appendChild(img);
+        })
+        .catch(function (error: any) {
+          console.error('oops, something went wrong!', error);
+        })
+    }
+      , 1000);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxEchartsModule } from 'ngx-echarts';
+import * as echarts from 'echarts/core';
+import {
+  TitleComponent,
+  TooltipComponent,
+  GridComponent
+} from 'echarts/components';
+// Import the Canvas renderer, note that introducing the CanvasRenderer or SVGRenderer is a required step
+import {
+  CanvasRenderer
+} from 'echarts/renderers';
+import { LineChart } from 'echarts/charts';
 
 import { AppComponent } from './app.component';
+import Marcaron from './marcaron';
+
+echarts.use(
+  [TitleComponent, TooltipComponent, GridComponent, LineChart, CanvasRenderer]
+);
+echarts.registerTheme('macarons', Marcaron);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxEchartsModule.forRoot({ echarts }),
   ],
   providers: [],
   bootstrap: [AppComponent]
